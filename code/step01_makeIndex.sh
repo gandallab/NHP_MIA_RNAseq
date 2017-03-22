c561ad3f704d78d0187bfda924cdb3a4b65a266b
Michael Gandal <mgandal@gmail.com>
2017-03-22 11:07:49 -0700

    Update and rename 01_makeIndex.sh to step01_makeIndex.sh

--- code/step01_makeIndex.sh
+++ code/step01_makeIndex.sh
@@ -2,8 +2,8 @@
 
 STARcall=/u/home/g/gandalm/bin/STAR-2.5.2b/bin/Linux_x86_64/STAR
 
-#Downloaded from: ftp://ftp.ensembl.org/pub/release-87/fasta/macaca_mulatta/dna/Macaca_mulatta.Mmul_8.0.1.dna_sm.toplevel.fa.gz
-GenomeFA=/u/home/g/gandalm/project-geschwind/RefGenome/Macaca_mulatta/Mmul_8/Sequence/WholeGenomeFasta/Macaca_mulatta.Mmul_8.0.1.dna_sm.toplevel.fa
+#Downloaded from: ftp://ftp.ensembl.org/pub/release-87/fasta/macaca_mulatta/dna/Macaca_mulatta.Mmul_8.0.1.dna.toplevel.fa.gz
+GenomeFA=/u/home/g/gandalm/project-geschwind/RefGenome/Macaca_mulatta/Mmul_8/Sequence/WholeGenomeFasta/Macaca_mulatta.Mmul_8.0.1.dna.toplevel.fa
 
 #Downloaded from:ftp://ftp.ensembl.org/pub/release-87/gtf/macaca_mulatta/Macaca_mulatta.Mmul_8.0.1.87.gtf.gz
 GTF=/u/home/g/gandalm/project-geschwind/RefGenome/Macaca_mulatta/Mmul_8/Annotation/Macaca_mulatta.Mmul_8.0.1.87.gtf
@@ -11,4 +11,4 @@ GTF=/u/home/g/gandalm/project-geschwind/RefGenome/Macaca_mulatta/Mmul_8/Annotati
 STARindex=/u/home/g/gandalm/project-geschwind/RefGenome/Macaca_mulatta/Mmul_8/Sequence/STARindex
 
 
-${STARcall} --runThreadN 8 --runMode genomeGenerate --genomeFastaFiles $GenomeFA --sjdbGTFfile $GTF --genomeDir $STARindex
+${STARcall} --runThreadN 1 --runMode genomeGenerate --genomeFastaFiles $GenomeFA --sjdbGTFfile $GTF --genomeDir $STARindex
